0e77d7d13c24fa0b15dc5db25668ddcd4679a9fd
shivpatel027 <patelshiv1701@gmail.com>
2024-06-29 08:19:16 -0500

    Strings started

---
+++ Strings/initialization.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class initialization {
+    public static void main(String args[]) {
+        char[] arr = {'a', 'b', 'c', 'd', 'e'};
+        String str = "abcde";
+        String st = new String("xyz");
+    }
+    
+    //Strings are IMMUTABLE - String can't be edited
+    
+}
