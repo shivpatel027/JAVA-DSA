3ea44e73d782625d1bb3ad718405a8c5c9eac27b
shivpatel027 <patelshiv1701@gmail.com>
2024-06-29 08:33:38 -0500

    Strirng I/O added

--- Strings/initialization.java
+++ Strings/initialization.java
@@ -1,12 +1,16 @@
 package Strings;
-
+import java.util.*;
 public class initialization {
     public static void main(String args[]) {
         char[] arr = {'a', 'b', 'c', 'd', 'e'};
         String str = "abcde";
         String st = new String("xyz");
+
+        //Strings are IMMUTABLE - String can't be edited
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
     }
     
-    //Strings are IMMUTABLE - String can't be edited
     
 }
