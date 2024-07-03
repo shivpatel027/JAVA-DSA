23600a932c7d0e51f42401db049d6aaad8233f45
shivpatel027 <patelshiv1701@gmail.com>
2024-07-03 16:03:31 -0500

    Clear the i th bit

---
+++ BitManipulation/clearIthBit.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class clearIthBit {
+    public static int clearIth(int n, int i) {
+        int bitMask = ~(1<<i);
+        return n&bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clearIth(10 , 1));
+    }
+}
