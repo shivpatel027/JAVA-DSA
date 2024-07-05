08fd2752afb044f5f24c9bdac0cb80c6c44240ee
shivpatel027 <patelshiv1701@gmail.com>
2024-07-05 14:40:21 -0500

    is power of 2 added

---
+++ BitManipulation/powerOf2.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class powerOf2 {
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n-1)) == 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
+}
