7b8ad633851f65ee9d036d5534dfd85760ee5d9b
shivpatel027 <patelshiv1701@gmail.com>
2024-07-13 14:17:26 -0500

    HashMap created

---
+++ HashMap/hashMap.java
@@ -0,0 +1,10 @@
+package HashMap;
+import java.util.HashMap;
+
+public class hashMap {
+    public static void main(String[] args) {
+        //create
+        HashMap<String, Integer> hm = new HashMap<>();
+
+    }
+}
