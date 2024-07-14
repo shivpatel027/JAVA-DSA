d763d8706c9edf2acd9a8d617632b6e083e91117
shivpatel027 <patelshiv1701@gmail.com>
2024-07-14 13:14:36 -0500

    ArrayList Operations added

--- ArrayList/arrayList.java
+++ ArrayList/arrayList.java
@@ -4,8 +4,36 @@ import java.util.ArrayList;
 public class arrayList {
     public static void main(String[] args) {
         //Java Colleaction Framework
-        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
         ArrayList<Boolean> list3 = new ArrayList<>();
+
+        // Add Operation
+        list.add(1); //O(1)
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.add(1,9);//O(n)
+
+        System.out.println(list);
+
+        //Get Operation
+        int element = list.get(2);
+        System.out.println(element);
+
+        //Delete Element
+        list.remove(2);
+        System.out.println(list);
+
+        //Set Elements
+        list.set(2,10);
+        System.out.println(list);
+
+        //Contais Elements
+        System.out.println(list.contains(1));
+        System.out.println(list.contains(12));
     }
 }
+ 
