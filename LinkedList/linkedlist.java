ac04c412d8366d313dd725779818403cfa0468fa
shivpatel027 <patelshiv1701@gmail.com>
2024-07-20 01:37:08 -0500

    remove operation added

--- LinkedList/linkedlist.java
+++ LinkedList/linkedlist.java
@@ -86,6 +86,29 @@ public class LinkedList {
         size--;
         return val;
     }
+
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("Linked List is empty");
+            return Integer.MAX_VALUE;
+        } else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size - 2;
+        Node prev = head;  
+        for(int i = 0; i<size-2; i++) {
+            prev = prev.next;
+        }
+
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev; 
+        size--;
+        return val;
+    }
     public static void main(String[] args) {
         LinkedList l = new LinkedList();
         l.addFirst(1);
@@ -96,6 +119,9 @@ public class LinkedList {
         l.print();
         l.removeFirst();
         l.print(); 
+
+        l.removeLast();
+        l.print();
         System.out.println(size);
     }
 }
